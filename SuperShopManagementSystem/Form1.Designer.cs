b9b9a48b47ab74ffe2672c8939d0a8d40a7d9619
nurgit <nurmohammad.aiub@gmail.com>
2019-11-21 01:11:56 +0600

    commit 3

--- SuperShopManagementSystem/Form1.Designer.cs
+++ SuperShopManagementSystem/Form1.Designer.cs
@@ -28,21 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.Welcome = new System.Windows.Forms.Label();
+            this.bCustomer = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // Welcome
+            // 
+            this.Welcome.AutoEllipsis = true;
+            this.Welcome.AutoSize = true;
+            this.Welcome.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Welcome.Font = new System.Drawing.Font("Microsoft Sans Serif", 25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Welcome.ForeColor = System.Drawing.Color.DarkBlue;
+            this.Welcome.Location = new System.Drawing.Point(344, 40);
+            this.Welcome.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
+            this.Welcome.Name = "Welcome";
+            this.Welcome.Size = new System.Drawing.Size(160, 45);
+            this.Welcome.TabIndex = 0;
+            this.Welcome.Text = "Welcome";
+            this.Welcome.UseCompatibleTextRendering = true;
+            // 
+            // bCustomer
+            // 
+            this.bCustomer.Cursor = System.Windows.Forms.Cursors.No;
+            this.bCustomer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.bCustomer.Location = new System.Drawing.Point(139, 197);
+            this.bCustomer.Margin = new System.Windows.Forms.Padding(6);
+            this.bCustomer.Name = "bCustomer";
+            this.bCustomer.Size = new System.Drawing.Size(178, 74);
+            this.bCustomer.TabIndex = 1;
+            this.bCustomer.Text = "Customer";
+            this.bCustomer.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            this.button2.Cursor = System.Windows.Forms.Cursors.No;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(344, 197);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 74);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Customer";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button3
+            // 
+            this.button3.Cursor = System.Windows.Forms.Cursors.No;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(571, 197);
+            this.button3.Margin = new System.Windows.Forms.Padding(6);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(178, 74);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "Customer";
+            this.button3.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(973, 594);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.bCustomer);
+            this.Controls.Add(this.Welcome);
+            this.Enabled = false;
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.CornflowerBlue;
+            this.Margin = new System.Windows.Forms.Padding(6);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.Text = "Super Shop Management System";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label Welcome;
+        private System.Windows.Forms.Button bCustomer;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
     }
 }
 
